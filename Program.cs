cfa99782825ea577f13d451624cf08a5fdf8a79e
Alex <best1972@list.ru>
2023-01-15 11:00:40 +0300

    made task 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B. 3, 5 -> 243 (3⁵), 2, 4 -> 16
+/*Console.Write("Введите число А: ");
+int Num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите натуральное число В: ");
+int Num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Число A в натуральной степени B равно: {GetDegree(Num1)}");
+//Метод --------------------------------------------------------
+int GetDegree(int Num1)
+{
+    int Degree = Num1;
+    for (int i = 1; i < Num2; i++)
+    {
+        Degree = Degree * Num1;
+    }
+    return Degree;
+}*/
