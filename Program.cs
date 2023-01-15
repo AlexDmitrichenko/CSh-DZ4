ab66f68172dcdf378f32ea6c9b3723cf8befbd53
Alex <best1972@list.ru>
2023-01-15 11:57:54 +0300

    save change

--- Program.cs
+++ Program.cs
@@ -15,3 +15,20 @@ int GetDegree(int Num1)
     }
     return Degree;
 }*/
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму 
+//цифр в числе. 452-> 11, 82-> 10, 9012-> 12
+/*Console.Write("Введите число: ");
+string Numb = Console.ReadLine()!;
+Console.WriteLine($"Сумма цифр в числе: {GetSum(Numb)}");
+//Метод --------------------------------------------------------
+int GetSum(string N)
+{
+    int size = Numb.Length;
+    int sum = Numb[0];
+    /*int FerstNumb = Numb[0];
+    for (int i = 1; i <= size; i++)
+    {
+        sum = FerstNumb + Numb[i];
+    }*/
+ //   return sum;
+//}
